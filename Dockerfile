433b601ccff58316db9d53dfb454ec903ec96511
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-08-27 09:29:38 +0300

    WIP: support versioning

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@
 FROM consbio/mbtileserver
 EXPOSE 8000
 RUN apk add --update curl
+ENV TILE_VERSION
 COPY start.sh start.sh
 ENTRYPOINT /start.sh
--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 
 set -eu
 
+DR_LINKKI_TILES_URL="https://jore4storage.blob.core.windows.net/jore4-ui/${TILE_VERSION}/dr_linkki.mbtiles"
+
 mkdir -p /tilesets
-curl https://jore4storage.blob.core.windows.net/jore4-ui/dr_linkki.mbtiles -o /tilesets/dr_linkki.mbtiles
+curl DR_LINKKI_TILES_URL -o /tilesets/dr_linkki.mbtiles
 
 ./mbtileserver
